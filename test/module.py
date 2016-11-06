cd3d30c8d2850961b1ca743d0a25d4f0e8cca8c3
John McElvenny <jlmcelv@clemson.edu>
2016-11-06 18:39:13 -0500

    Update module.py

--- test/module.py
+++ test/module.py
@@ -2,9 +2,13 @@
 
 #####PYTHON PAGE FORMAT
 # GET /test/module.py?user=bob&pass=jones HTTP/1.1
-# The file /test/module.py has method onPageLoad() imported
-# onPageLoad(user=bob&pass=jones) is invoked
+#
+# Server imports module.py's onPageLoad method
+# onPageLoad(user=bob&pass=jones) is called
 # onPageLoad() returns a string: the contents of the webpage.
+#
+# Side Note: 	Python requires that an __init__.py file exist in the directory of the .py file
+#		This is to ensure safe imports. Not necessary if the .py is the same directory as server.py
 #####
 
 def onPageLoad(params):
